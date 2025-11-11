0320b2bbebb332e8af63aa3420dd730b84df86f2
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-12 04:24:43 +0600

    ready for vercel deploy

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const serviceAccount = require("./local-food-lovers-client-firebase-admin-key.json");
+// const serviceAccount = require("./local-food-lovers-client-firebase-admin-key.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
