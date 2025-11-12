bef7c902bfed3ecc50ea04d4556e3db043a547ed
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-12 16:41:11 +0600

    client connect commented

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const db = client.db("localFoodLovers");
         const usersCollection = db.collection("users");
         const reviewsCollection = db.collection("reviews");
